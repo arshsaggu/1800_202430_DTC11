1ee9aa1c3280395eea1d9d36573b88f52ad0b877
arshsaggu <asaggu10@my.bcit.ca>
2024-11-01 18:14:14 -0700

    quotes added for every day and a new function added so that quotes change automatically according to the day the user logs in

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,33 @@ function getNameFromAuth() {
     });
 }
 getNameFromAuth(); //run the function
+
+
+// Function to get the current day of the week as a string (e.g., "monday", "tuesday")
+function getCurrentDay() {
+    const daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    const today = new Date();
+    return daysOfWeek[today.getDay()]; 
+}
+
+// Function to read the quote of the day from the Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote() {
+    const day = getCurrentDay()
+    db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
+        .onSnapshot(dayDoc => {                                                              //arrow notation
+            console.log("current document data: " + dayDoc.data());                          //.data() returns data object
+            document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
+
+            //Here are other ways to access key-value data fields
+            //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+            //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+            //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+
+        }, (error) => {
+            console.log("Error calling onSnapshot", error);
+        });
+}
+readQuote();        //calling the function
+
+
