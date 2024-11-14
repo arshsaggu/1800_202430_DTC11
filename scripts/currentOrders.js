adc43a652aada372e6dfcdc434b4c8e6b4d5acbe
Adnan-Abdulle <adnan.abdulle43@gmail.com>
2024-11-13 21:20:32 -0800

    snapshot the info from firestore

--- scripts/currentOrders.js
+++ scripts/currentOrders.js
@@ -7,4 +7,18 @@ function CurrentOrder() {
             return; // Abort the function if the user is not logged in
         }
 
-    })}
+        const userId = user.uid; 
+        // Reference to the user's 'userOrders' sub-collection
+        const orderRef = db.collection("orders").doc(userId).collection("userOrders");
+
+        orderRef.orderBy("orderTime", "desc").limit(1).get()
+            .then(snapshot => {
+                if (!snapshot.empty) {
+               
+                    const orderData = snapshot.docs[0].data();
+                    const orderId = snapshot.docs[0].id; 
+                    const status = orderData.status;
+                    const orderTime = orderData.orderTime.toDate().toLocaleString(); 
+                    const totalPrice = orderData.price;
+                    const title = orderData.title;
+                    
