0debb0d3035e8b921b8d9f33f4636f5ea7bdee6c
Adnan-Abdulle <adnan.abdulle43@gmail.com>
2024-11-19 00:04:50 -0800

    edit currentorder function

--- scripts/currentOrders.js
+++ scripts/currentOrders.js
@@ -49,14 +49,7 @@ function CurrentOrder() {
 
                 } else {
                     document.getElementById("currentOrderSection").innerHTML = `
-                        <div class="col-md-4 mb-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title text-xl font-bold">No current order</h5>
-                                    <p class="card-text">You don't have any orders in preparation right now.</p>
-                                </div>
-                            </div>
-                        </div>
+                        <h2 class="text-center">You have no current orders.</h2>
                     `;
                 }
             })
@@ -151,7 +144,7 @@ function PastOrders() {
                                     <p class="card-text">Status: Delivered</p>
                                     <p class="card-text">Time Delivered: ${orderTime}</p>
                                     <p class="card-text">Total Price: $${totalPrice}</p>
-                                     <button class="confirm-btn mt-4 text-sm py-2 px-4 rounded-xl bg-gradient-to-b from-orange-300 to-orange-400 text-white w-20 md:w-auto h-10 shadow-md hover:shadow-lg border-b-4 border-orange-500 transform hover:translate-y-1 transition duration-200 ease-in-out ml-4" style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);">Confirm Delivery</button>
+
                                 </div>
                             </div>
                         </div>
