600c37667c0e1933254e11a26924fcf18a0815a7
arshsaggu <asaggu10@my.bcit.ca>
2024-11-29 10:55:13 -0800

    some changes to the currentorder.js

--- scripts/currentOrders.js
+++ scripts/currentOrders.js
@@ -21,17 +21,17 @@ function CurrentOrder() {
                     const totalPrice = orderData.price;
                     const orderImg = orderData.foodCode;
                     const title = orderData.title;
-
+                    
                     // Create the current order card dynamically
                     const orderCard = `
                         
                             <div class="card py-2 mx-2" style="width: 24rem" >
                                 <img src="./images/${orderImg}.jpg" class="card-image card-img-top" alt="Current Order">
                                 <div class="card-body">
-                                    <h5 class="card-title text-2xl">${title}</h5>
+                                    <h5 class="card-title text-2xl font-semibold">${title}</h5>
                                     <p class="card-text mt-0">Status: ${status}</p>
                                     <p class="card-text mt-0">Estimated delivery: ${orderTime}</p>
-                                    <p class="">Total Price: $${totalPrice}</p>
+                                    <p class="">$${totalPrice}</p>
                                      <div class="flex justify-content-left mt-2 ">
                                      <button class="confirm-btn text-md py-2 px-4 rounded-xl bg-gradient-to-b from-orange-300 to-orange-400 text-white w-20 md:w-auto h-10 shadow-md hover:shadow-lg border-b-4 border-orange-500 transform hover:translate-y-1 transition duration-200 ease-in-out"
                                      style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);">
@@ -164,7 +164,7 @@ function PastOrders() {
                  <div class="card py-2 mx-2" style="width: 24rem" >
                                 <img src="./images/${orderImg}.jpg" class="card-image card-img-top" alt="Current Order">
                                 <div class="card-body">
-                                    <h5 class="card-title text-2xl">${title}</h5>
+                                    <h5 class="card-title text-2xl font-semibold">${title}</h5>
                                     <p class="card-text mt-0">Status: Delivered </p>
                                     <p class="card-text mt-0">Time Delivered: ${orderTime}</p>
                                     <p class="">Total Price: $${totalPrice}</p>
@@ -181,6 +181,7 @@ function PastOrders() {
             }).catch(error => {
                 console.error("Error fetching past orders: ", error);
                 alert("Failed to fetch past orders.");
+                
             });
     }
     )
