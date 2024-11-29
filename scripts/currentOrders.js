b26b5c0bd6e588f3fc319a00746491960b269f43
arshsaggu <asaggu10@my.bcit.ca>
2024-11-29 11:00:23 -0800

    few minor changes

--- scripts/currentOrders.js
+++ scripts/currentOrders.js
@@ -29,9 +29,9 @@ function CurrentOrder() {
                                 <img src="./images/${orderImg}.jpg" class="card-image card-img-top" alt="Current Order">
                                 <div class="card-body">
                                     <h5 class="card-title text-2xl font-semibold">${title}</h5>
-                                    <p class="card-text mt-0">Status: ${status}</p>
-                                    <p class="card-text mt-0">Estimated delivery: ${orderTime}</p>
-                                    <p class="">$${totalPrice}</p>
+                                    <p class="card-text text-lg mt-0">Status: ${status}</p>
+                                    <p class="card-text text-lg mt-0">Estimated delivery: ${orderTime}</p>
+                                    <p class="font-semibold">$${totalPrice}</p>
                                      <div class="flex justify-content-left mt-2 ">
                                      <button class="confirm-btn text-md py-2 px-4 rounded-xl bg-gradient-to-b from-orange-300 to-orange-400 text-white w-20 md:w-auto h-10 shadow-md hover:shadow-lg border-b-4 border-orange-500 transform hover:translate-y-1 transition duration-200 ease-in-out"
                                      style="box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);">
@@ -165,9 +165,9 @@ function PastOrders() {
                                 <img src="./images/${orderImg}.jpg" class="card-image card-img-top" alt="Current Order">
                                 <div class="card-body">
                                     <h5 class="card-title text-2xl font-semibold">${title}</h5>
-                                    <p class="card-text mt-0">Status: Delivered </p>
-                                    <p class="card-text mt-0">Time Delivered: ${orderTime}</p>
-                                    <p class="">Total Price: $${totalPrice}</p>
+                                    <p class="card-text text-lg mt-0">Status: Delivered </p>
+                                    <p class="card-text text-lg mt-0">Time Delivered: ${orderTime}</p>
+                                    <p class="font-semibold">$${totalPrice}</p>
                                      
                             </div>
                             </div>
